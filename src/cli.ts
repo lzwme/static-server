52616f0afad9f1bdc7f7ab1a7f13ada01cc0dbcb
renxia <lzwy0820@qq.com>
2022-09-17 22:02:51 +0800

    fix: 修正 cli.ts 引用 chalk 导致的异常

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,5 @@
 import { program } from 'commander';
-import chalk from 'chalk';
+import { color } from 'console-log-colors';
 import { initServer } from './static-server';
 
 const pkg = require('../package.json');
@@ -7,8 +7,8 @@ const pkg = require('../package.json');
 program
   .aliases(['ss'])
   .version(pkg.version, '-v, --version')
-  .description(chalk.yellow(pkg.description) + ` [version@${chalk.cyanBright(pkg.version)}]`)
-  // .option('-c, --config-path [filepath]', `配置文件 ${chalk.yellow(config.configPath)} 的路径`)
+  .description(color.yellow(pkg.description) + ` [version@${color.cyanBright(pkg.version)}]`)
+  // .option('-c, --config-path [filepath]', `配置文件 ${color.yellow(config.configPath)} 的路径`)
   .option('-d, --base-dir [baseDir]', '静态服务器的根目录路径')
   .option('-p, --port [port]', '端口号')
   .option('-o, --open', '启动后是否打开静态服务器首页')
